69b76a1c1e16b70f5a010db0900e2a3a82eeb447
virat1221 <115089699+virat1221@users.noreply.github.com>
2024-10-09 00:00:27 +0530

    Add files via upload

---
+++ http-server/index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000; 
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+http
+    .createServer((request, response) => {
+    let url = request.url;
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      switch (url) {
+        case "/project":
+            response.write(projectContent);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationContent);
+            response.end();
+            break;
+
+        default:
+            response.write(homeContent);
+            response.end();
+            break;
+      }
+    })
+    .listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    });
